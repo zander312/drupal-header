34a61bee8b5885e3731ec3de3be192552afe8802
menuka-sba <menuka.samaranayake@sba.gov>
2017-01-24 15:57:35 -0500

    testing stuff

--- header.js
+++ header.js
@@ -1,25 +1,61 @@
-jQuery.noConflict();
+// jQuery.noConflict();
+//
+// (function($){
+// $(document).ready(function(){
+//   $(".menu-btn-new").click(function(e){
+//     e.preventDefault();
+//     $(".main-nav-new").slideToggle();
+//     $(".menu-icon-hamburger-new").toggle();
+//     var btnText = $(".menu-btn-text-new").text();
+//     $(".menu-btn-text-new").text(btnText == "CLOSE" ? "MENU" : "CLOSE");
+//     $(".menu-icon-close-new").toggle();
+//     $(".menu-btn-text-new").css({"color": btnText == "CLOSE" ? "#0B97DD" : "#006BA2"});
+//     $(".menu-btn-new").css({"border": btnText == "CLOSE" ? "0.533vw solid #0B97DD" : "0.533vw solid #006BA2"});
+//   })
+//
+//   $(".search-input-field-new").on('keydown', function(e) {
+//     if (e.keyCode == 13) {
+//       e.preventDefault();
+//       var searchParam = $(".search-input-field-new").val()
+//       var searchRes = encodeURI("/tools/search-result-page?search=" + searchParam)
+//       window.location.href = (searchRes);
+//     }
+//   })
+//
+// });
+// })(jQuery);
 
-(function($){
+
+
+
 $(document).ready(function(){
-  $(".menu-btn-new").click(function(e){
+  $(".menuBtnNew").click(function(e){
     e.preventDefault();
-    $(".main-nav-new").slideToggle();
-    $(".menu-icon-hamburger-new").toggle();
-    var btnText = $(".menu-btn-text-new").text();
-    $(".menu-btn-text-new").text(btnText == "CLOSE" ? "MENU" : "CLOSE");
-    $(".menu-icon-close-new").toggle();
-    $(".menu-btn-text-new").css({"color": btnText == "CLOSE" ? "#0B97DD" : "#006BA2"});
-    $(".menu-btn-new").css({"border": btnText == "CLOSE" ? "0.533vw solid #0B97DD" : "0.533vw solid #006BA2"});
+    $(".mainNavNew").toggle("slide", {direction:'right'});
+    $(".menuIconHamburgerNew").toggle();
+    var btnText = $(".menuBtnTextNew").text();
+    $(".menuBtnTextNew").text(btnText == "CLOSE" ? "MENU" : "CLOSE");
+    $(".menuIconCloseNew").toggle();
+    $(".menuBtnNew").css({"color": btnText == "CLOSE" ? "#0B97DD" : "#006BA2"});
+    $(".menuBtnNew").css({"border": btnText == "CLOSE" ? "0.533vw solid #0B97DD" : "0.533vw solid #006BA2"});
+    // $(".test").animate({"margin-top": btnText == "CLOSE" ? "16vw" : "161vw"})
+
+    var height = $(".mainNavNew").height();
+    if ($(".mainNavNew").is(":visible")){
+    }
   })
 
-  $(".search-input-field-new").live('keydown', function(e) {
+  $(".searchInputFieldNew").on('keydown', function(e) {
     if (e.keyCode == 13) {
       e.preventDefault();
-      var searchParam = $(".search-input-field-new").val()
-      var searchRes = encodeURI("/tools/search-result-page?search=" + searchParam)
-      window.location.href = (searchRes);
+      var searchParam = $(".searchInputFieldNew").val()
+      window.location.href = (createSearchURI(searchParam));
     }
   })
 });
-})(jQuery);
+
+
+function createSearchURI(searchParam){
+  var searchRes = encodeURI("/tools/search-result-page?search=" + searchParam)
+  return searchRes
+}
