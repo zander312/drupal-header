a85d23284fda36492203da34592edac613f24500
zander312 <iskander93@gmail.com>
2017-01-31 15:25:51 -0500

    adds jquery to display, search and hide search bar

--- header.js
+++ header.js
@@ -3,6 +3,35 @@ $(document).ready(function(){
 
   focusInMenu();
 
+  $("#search-bar").hide();
+  
+  $("#search-bar").submit(function (e) {
+    e.preventDefault()
+    var searchValue = $("#search-input").val();
+    var searchURI = createSearchURI(searchValue)
+    window.location = searchURI
+  })
+  
+  $(document).on('click', function(e){
+    console.log(e.target);
+    if(e.target.id == "search-toggle"){
+      $("#search-toggle").hide();
+      $("#search-bar").show();
+    }
+    else if (e.target.id == "search-btn"){
+      console.log("search")
+      var searchValue = $("#search-input").val();
+      var searchURI = createSearchURI(searchValue)
+      window.location = searchURI
+    }
+    else if (e.target.id !== "search-btn" && e.target.id !== "search-input"){
+      $("#search-bar").hide();
+      $("#search-toggle").show();
+    }
+  });
+
+
+
 
   //mobile jquery
   $(".menuBtnNew").click(function(e){
@@ -26,7 +55,7 @@ $(document).ready(function(){
 });
 
 function createSearchURI(searchParam){
-  var searchRes = encodeURI("/tools/search-result-page?search=" + searchParam)
+  var searchRes = encodeURI("https://www.sba.gov/tools/search-result-page?search=" + searchParam)
   return searchRes
 }
 
@@ -36,4 +65,17 @@ function focusInMenu(){
     $(this).siblings(".main-menu-new").css("visibility", "visible")
   });
 }
+
+function toggleSearch(){
+
+}
+
+// function submitSearch(){
+//   $("#search-btn").click( function(){
+//     console.log("oioioioioioioioi")
+//   });
+// }
+
+// ("<input type='text' name='search-bar' placeholder='Search'><a id='search-btn'><i tabindex=0 class='fa fa-search remove-a-style' aria-hidden='true'></i></a>")
+
 
